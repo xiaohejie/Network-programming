49a09ca7ea440c8469008e85a8390d6e3ec369ce
xiaoajie <2234842317@qq.com>
2023-12-18 11:07:04 +0800

    实现优雅退出

--- day13-LogicServer/day13-LogicServer/day13-LogicServer.cpp
+++ day13-LogicServer/day13-LogicServer/day13-LogicServer.cpp
@@ -10,12 +10,36 @@ bool bstop = false;
 std::condition_variable cond_quit;
 std::mutex mutex_quit;
 
+void sig_handler(int sig) {
+	if (sig == SIGINT || sig == SIGTERM) {
+		std::unique_lock<std::mutex> lock_quit(mutex_quit);	//加锁，配合条件变量
+		bstop = true; 
+		cond_quit.notify_one();	//唤醒条件变量
+	}
+}
+
 int main()
 {
 	try {
 		boost::asio::io_context  io_context;
-		boost::asio::signal_set signals(io_context, SIGINT, SIGTERM);
-		signals.async_wait([&io_context](auto, auto) {
+		/*
+		//通过C的方式
+		std::thread net_work_thread([&io_context]() {	//另开一个线程处理，主线程处于挂起状态
+			CServer s(io_context, 10086);
+			io_context.run();
+			});
+		 //监听信号
+		signal(SIGINT, sig_handler);
+		signal(SIGTERM, sig_handler);
+		while(!bstop){
+			std::unique_lock<std::mutex> lock_quit(mutex_quit);	
+			cond_quit.wait(lock_quit);
+		}
+		io_context.stop(); 
+		net_work_thread.join();*/
+		//通过boost::asio实现退出
+		boost::asio::signal_set signals(io_context, SIGINT, SIGTERM);	//捕获想捕获得信号，然后想注册到哪个服务里
+		signals.async_wait([&io_context](auto, auto) {	//异步等待
 			io_context.stop();
 			});
 		CServer s(io_context, 10086);
